624cf719e4f3f0447de6f4d75fa270f0399ea2a3
PasqualBruno <pasqualbruno97@gmail.com>
2025-09-13 13:57:57 -0300

    feat- home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,40 @@
-import { Text, View } from "react-native"
+import { images, offers } from "@/constants"
+import cn from "clsx"
+import React, { Fragment } from "react"
+import { FlatList, Image, Pressable, Text, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
 
 const Index = () => {
   return (
-    <View>
-      <Text className="text-red-800">Index </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+
+
+
+      <FlatList className="w-full " data={offers} renderItem={({ item, index }) => {
+        const isEven: boolean = index % 2 === 0
+
+        return (
+          <View >
+            <Pressable className={cn("offer-card", isEven ? "flex-row-reverse" : "flex-row")}
+              style={{ backgroundColor: item.color }}
+              android_ripple={{ color: "#fffff22" }}>
+              {(pressed) => (<Fragment>
+                <View className="h-full w-1/2">
+                  <Image source={item.image} className="size-full" resizeMode="contain" />
+                </View>
+                <View className={cn("offer-card__info", isEven ? "pl-10" : "pr-10")}>
+                  <Text className="h1-bold text-white leading-tight">
+                    {item.title}
+                  </Text>
+                  <Image source={images.arrowRight} className="size-10" resizeMode="contain" tintColor={"#fffff"} />
+                </View>
+              </Fragment>)}
+            </Pressable>
+          </View>)
+      }
+      } contentContainerClassName="pb-28 px-5" />
+
+    </SafeAreaView>
   )
 }
 
