983f6f483a0ca84a0d64e0f9895ee32ea0bc47e6
PasqualBruno <pasqualbruno97@gmail.com>
2025-09-13 13:58:20 -0300

    feat- home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,23 @@
 import { images, offers } from "@/constants"
 import cn from "clsx"
 import React, { Fragment } from "react"
-import { FlatList, Image, Pressable, Text, View } from "react-native"
+import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 
 const Index = () => {
   return (
     <SafeAreaView className="flex-1 bg-white">
 
-
+      <View className="flex-between flex-row w-full my=5  px-5">
+        <View className="flex-start">
+          <Text className="small-bold text-primary"> DELIVER TO</Text>
+          <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+            <Text className="paragraph-bold text-dark-100" >Brasil</Text>
+            <Image source={images.arrowDown} className="size-3" resizeMode="contain" />
+          </TouchableOpacity>
+        </View>
+        <Text>Cart</Text>
+      </View>
 
       <FlatList className="w-full " data={offers} renderItem={({ item, index }) => {
         const isEven: boolean = index % 2 === 0
