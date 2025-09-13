0d9969c0f2cc52a6835b95258af075f640931f24
PasqualBruno <pasqualbruno97@gmail.com>
2025-09-13 09:24:39 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const Index = () => {
+  return (
+    <View>
+      <Text className="text-red-800">Index </Text>
+    </View>
+  )
+}
+
+export default Index
