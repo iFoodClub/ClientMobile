d2e87ab5cec7fb84b236971c7bd55910af7378c7
PasqualBruno <pasqualbruno97@gmail.com>
2025-09-15 23:02:48 -0300

    appwrite config

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-    plataform: "com.fastfood.foodordering"
+    plataform: "com.fastfood.foodordering",
+    databaseId: '68c8c1c7003c51745018',
+    userCollectionId: 'user'
 }
