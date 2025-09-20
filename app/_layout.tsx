99d4f2dc9f4bbfb8b97ad28b1ee42fe9d52af6c0
PasqualBruno <pasqualbruno97@gmail.com>
2025-09-20 09:41:43 -0300

    feat: basic navigation between screens and new dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,24 @@
 import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
 import "./global.css";
-
+import { useAuthStore } from "@/src/store/authStore";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const { isLoggedIn, shouldCreateAccount } = useAuthStore();
 
   return (
-    <Stack />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={isLoggedIn}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Protected guard={!isLoggedIn && !shouldCreateAccount}>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Protected guard={!isLoggedIn && shouldCreateAccount}>
+        <Stack.Screen name="create-account" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+    </Stack>
   );
 }
