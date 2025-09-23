9290cf950d7560448f0be0bdf329c48953cd50ce
PasqualBruno <pasqualbruno97@gmail.com>
2025-09-23 09:02:16 -0300

    feat: working in the auth screens, new components

---
+++ components/Button/Button.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+type ButtonProps = {
+  onPress: () => void;
+  text: string;
+  disabled?: boolean;
+};
+
+const Button = ({ onPress, text, disabled = false }: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      disabled={disabled}
+      onPress={onPress}
+      className={`px-4 py-4 ${
+        disabled ? "bg-gray-400" : "bg-primary"
+      } rounded-lg flex items-center w-full`}
+    >
+      <Text className="text-white font-semibold text-body">{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
