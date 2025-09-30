3f0a75713d229beb0786c5aefb9892cf2ea19280
PasqualBruno <pasqualbruno97@gmail.com>
2025-09-30 19:28:41 -0300

    feat: checkpoint - working in the bottom menu

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,12 +5,39 @@ import React from "react";
 const TabsLayout = () => {
   const { user, isRestaurant, isCompany, isEmployee } = useAuthStore();
 
+  console.log("isrestaurant", isRestaurant);
+  console.log("iscompany", isCompany);
+  console.log("isemployee", isEmployee);
+  console.log("user", user?.userType);
+
   return (
     <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="orders" options={{ title: "Orders" }} />
-      <Tabs.Screen name="dishes" options={{ title: "Dishes" }} />
+
+      <Tabs.Screen
+        name="dishes"
+        options={{
+          title: "Dishes",
+          href: isRestaurant ? "/dishes" : null,
+        }}
+      />
+
+      <Tabs.Screen
+        name="orders"
+        options={{
+          title: "Orders",
+        }}
+      />
+
       <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+
+      <Tabs.Screen
+        name="employees"
+        options={{
+          title: "Employees",
+          href: isCompany ? "/employees" : null,
+        }}
+      />
     </Tabs>
   );
 };
