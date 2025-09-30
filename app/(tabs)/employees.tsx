85e54b92094195069068ce6135938a779f92b0b6
PasqualBruno <pasqualbruno97@gmail.com>
2025-09-30 19:28:45 -0300

    feat: checkpoint - working in the bottom menu

---
+++ app/(tabs)/employees.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const employees = () => {
+  return (
+    <View>
+      <Text>employees</Text>
+    </View>
+  );
+};
+
+export default employees;
