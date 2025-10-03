07c165680ab41df1ef538be8633ec0a7166ad3ac
PasqualBruno <pasqualbruno97@gmail.com>
2025-10-03 20:46:06 -0300

    feat: auth screens and start of the restaurant screens

---
+++ components/Restaurant/Components/RestaurantMenu/RestaurantMenu.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+type RestaurantMenuProps = {
+  restaurantId: number;
+};
+
+const RestaurantMenu = ({ restaurantId }: RestaurantMenuProps) => {
+  async function fetchRestaurantMenu(restaurantId: number) {}
+
+  return (
+    <View>
+      <Text>RestaurantMenu</Text>
+    </View>
+  );
+};
+
+export default RestaurantMenu;
