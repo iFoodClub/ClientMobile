ebbf5bb54e670aad7a201fdb5ee1d44cedfbe3e8
PasqualBruno <pasqualbruno97@gmail.com>
2025-10-05 11:37:09 -0300

    feat: restaurant screens

--- app/(tabs)/dishes.tsx
+++ app/(tabs)/dishes.tsx
@@ -1,15 +1,20 @@
 import Button from "@/components/Button/Button";
 import { useAuthStore } from "@/src/store/authStore";
 import React from "react";
-import { Text } from "react-native";
+import { Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const dishes = () => {
   const { user } = useAuthStore();
 
+  console.log(JSON.stringify(user, null, 2));
+
   return (
     <SafeAreaView>
-      <Text>dishes</Text>
+      <Image
+        className="w-full h-40 bg-cover"
+        source={{ uri: user?.profileImage }}
+      />
       <Button text="Novo prato" onPress={() => {}} />
     </SafeAreaView>
   );
