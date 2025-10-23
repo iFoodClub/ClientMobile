260f59c6ae0e2541ccd7078f45c60c02414807fd
PasqualBruno <pasqualbruno97@gmail.com>
2025-10-22 22:03:29 -0300

    feat: orders starting

--- app/(tabs)/orders.tsx
+++ app/(tabs)/orders.tsx
@@ -1,11 +1,11 @@
+import PageHeader from "@/components/PageHeader/PageHeader";
 import React from "react";
-import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const orders = () => {
   return (
     <SafeAreaView>
-      <Text>orders</Text>
+      <PageHeader title="Pedidos" subtitle="Gerencie os pedidos recebidos " />
     </SafeAreaView>
   );
 };
